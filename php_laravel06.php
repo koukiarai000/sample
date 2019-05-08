d1236c40e10524a388c5dd489864ddd4113e9468
koukiarai <acidcherry373@gmail.com>
2019-05-08 17:54:08 +0900

    課題６

---
+++ php_laravel06.php
@@ -0,0 +1,6 @@
+//課題３
+Route::get('XXX','Admin\AAAController@bbb');
+
+//課題４
+Route::get('admin/profile/create','Admin\ProfileController@add');
+Route::get('admin/profile/edit','Admin\ProfileController@edit');
