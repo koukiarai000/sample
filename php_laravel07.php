c823491d973bcd08a9307d59e0f13b810d933dc0
koukiarai <acidcherry373@gmail.com>
2019-05-09 16:58:32 +0900

    secont commit

---
+++ php_laravel07.php
@@ -0,0 +1,36 @@
+// resource/viewの配下にadmin/profileパスを作成
+mkdir -p admin/profile
+
+//add Actionのbladeファイルを作成
+touch create.blade.php
+
+//edit Actionのbladeファイルを作成
+touch edit.blade.php
+
+//create.blame.phpの中身
+<!DOCTYPE>
+<html>
+     <head>
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edeg">
+       <mata name="viewport" content="width=device-width",initial-scale="1">
+       <title>Mynews</title>
+     </head>
+     <body>
+       <h1>Myニュース作成画像</h1>
+     </body>
+</html>
+
+//edit.blame.phpの中身
+<!DOCTYPE>
+<html>
+     <head>
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edeg">
+       <mata name="viewport" content="width=device-width",initial-scale="1">
+       <title>Mynews</title>
+     </head>
+     <body>
+       <h1>Myニュース編集画面</h1>
+     </body>
+</html>
